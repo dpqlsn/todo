2fc86c82b649c9c420b6f280f91516743093244d
dpqlsn <a01037111209@gmail.com>
2025-04-21 10:10:45 +0900

    reset

---
+++ todo/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../src/App.css'
+import Header from './component/Header.jsx'
+import Editor from './component/Editor.jsx'
+
+
+function App(){
+  return (
+    <>
+  TodoList
+  <Header />
+  <Editor />
+  </>
+  )
+  
+}
+
+export default App
